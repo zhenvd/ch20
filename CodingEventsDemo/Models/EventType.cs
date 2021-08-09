1008e6e3208fa7a2029b6ce25f510a8c1c193a6b
Vincent Zhen <Vincent.D.Zhen@Outlook.com>
2021-08-09 11:40:19 -0400

    first commit

---
+++ CodingEventsDemo/Models/EventType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CodingEventsDemo.Models
+{
+    public enum EventType
+    {
+        Conference,
+        Meetup,
+        Workshop,
+        Social
+    }
+}
---
+++ CodingEventsDemo/ViewModels/AddEventViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using CodingEventsDemo.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CodingEventsDemo.ViewModels
+{
+    public class AddEventViewModel
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 50 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
+        [StringLength(500, ErrorMessage = "Description too long!")]
+        public string Description { get; set; }
+
+        [EmailAddress]
+        public string ContactEmail { get; set; }
+
+        public EventType Type { get; set; }
+
+        public List<SelectListItem> EventTypes { get; set;  } = new List<SelectListItem>
+        {
+            new SelectListItem(EventType.Conference.ToString(), ((int)EventType.Conference).ToString()),
+            new SelectListItem(EventType.Meetup.ToString(), ((int)EventType.Meetup).ToString()),
+            new SelectListItem(EventType.Social.ToString(), ((int)EventType.Social).ToString()),
+            new SelectListItem(EventType.Workshop.ToString(), ((int)EventType.Workshop).ToString())
+        };
+
+    }
+}
